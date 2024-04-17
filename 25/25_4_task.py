641c8a923ab7009fcb8fda237b1aacbb6b096370
Ivan Lvov <banoleb@mail.ru>
2024-04-17 16:19:23 +0300

    25_4

--- 25/25_4_task.py
+++ 25/25_4_task.py
@@ -1,15 +1,32 @@
 from sys import argv
 import sqlite3
 
+
+
 def get_data(select=None, params=None):
     connection = sqlite3.connect('dhcp_snooping.db')
     cursor = connection.cursor()
+    list_ok=[]
+    list_down=[]
     if select==None and params==None:
         for row in cursor.execute('select * from dhcp'):
-            print(row)
+            if row[5]==1:
+                list_ok.append(row)
+            else:
+                list_down.append(row)
     else:
+        print("Информация об устройствах с такими параметрами", select, params)
         for row in cursor.execute('select * from dhcp where '+select+' = '+params):
-            print(row)   
+            if row[5]==1:
+                list_ok.append(row)
+            else:
+                list_down.append(row)
+
+    print("Активные записи:")
+    print(list_ok)
+    print("Неактивные записи:")
+    print(list_down)
+
 
 list_a =["mac", "ip", "vlan", "interface", "switch"]
 arggs= len(argv)
@@ -22,3 +39,6 @@ elif arggs==3:
         print("Допустимые значения параметров: mac, ip, vlan, interface, switch")
 else:
     print("Пожалуйста, введите два или ноль аргументов")
+
+
+
