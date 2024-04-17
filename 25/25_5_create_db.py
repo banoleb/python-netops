0aa56e93f2145e746e8aecb1fb871a2041824f60
Ivan Lvov <banoleb@mail.ru>
2024-04-17 16:32:48 +0300

    25_5

---
+++ 25/25_5_create_db.py
@@ -0,0 +1,26 @@
+import sqlite3
+import os
+
+# Путь к файлу базы данных SQLite
+db_file = 'dhcp_snooping.db'
+
+# Проверяем, существует ли файл базы данных
+if not os.path.exists(db_file):
+    # Если файл не существует, создаем базу данных и загружаем схему
+    conn = sqlite3.connect(db_file)
+    cursor = conn.cursor()
+
+    # Чтение схемы из файла
+    with open('dhcp_snooping_schema_5.sql', 'r') as f:
+        schema = f.read()
+
+    # Выполнение схемы
+    cursor.executescript(schema)
+
+    # Сохранение изменений и закрытие соединения
+    conn.commit()
+    conn.close()
+
+    print("Создаю базу данных...")
+else:
+    print("База данных существует")
